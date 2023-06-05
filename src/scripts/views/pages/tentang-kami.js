8b5de63ce5c397c99d94fd7ed73745ef3587d94e
yondan-ryz <yonathandani76@gmail.com>
2023-06-05 19:19:47 +0700

    update about us

--- src/scripts/views/pages/tentang-kami.js
+++ src/scripts/views/pages/tentang-kami.js
@@ -1,39 +1,47 @@
+const createDeveloperItemTemplate = (developer) => `
+  <div class="dev-item">
+    <div class="dev-item__header">
+      <img class="dev-item__header__poster" src="${developer.photo}" alt="${developer.name}">
+      <div class="dev-item__header__rating">
+        <p>⭐️<span class="dev-item__header__rating__score">${developer.skills}</span></p>
+      </div>
+    </div>
+    <div class="dev-item__content">
+      <h3>${developer.name}</h3>
+    </div>
+  </div>
+`;
+
 const AboutUs = {
   async render() {
     return `
       <div class="content">
         <h2 class="content__heading">About Us</h2>
-        <div class="list-dev">
-          <div id="listdev"></div>
-        </div>
+        <div id="devs" class="devs">
+          ${this.generateDeveloperItems()}
+        </div>  
       </div>
     `;
   },
 
   async afterRender() {
-    const listDev = document.querySelector('#listdev');
+    // Tidak ada fitur pencarian
+  },
 
+  generateDeveloperItems() {
     const developers = [
-      { name: 'John Doe', position: 'Front-end Developer', image: './images/logo.png' },
-      { name: 'Jane Smith', position: 'Back-end Developer', image: './images/logo.png' },
-      { name: 'Alex Johnson', position: 'UI/UX Designer', image: './images/logo.png' },
-      { name: 'Alex Johnson 2', position: 'UI/UX Designer', image: './images/logo.png' },
+      { name: 'Developer 1', photo: './images/logo.png', skills: 'Frontend' },
+      { name: 'Developer 2', photo: './images/logo.png', skills: 'Backend' },
+      { name: 'Developer 3', photo: './images/logo.png', skills: 'Full Stack' },
+      { name: 'Developer 3', photo: './images/logo.png', skills: 'Full Stack' },
     ];
 
+    let developerItems = '';
     developers.forEach((developer) => {
-      const card = document.createElement('div');
-      card.classList.add('developer-card');
-      card.innerHTML = `
-        <div class="developer-card__image">
-          <img src="${developer.image}" alt="${developer.name}">
-        </div>
-        <div class="developer-card__content">
-          <h3>${developer.name}</h3>
-          <p>${developer.position}</p>
-        </div>
-      `;
-      listDev.appendChild(card);
+      developerItems += createDeveloperItemTemplate(developer);
     });
+
+    return developerItems;
   },
 };
 
